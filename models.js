720641ea5d39f8677b88dea643f34a4f6f4e1937
Jack Kinsey <jack.w.kinsey@gmail.com>
2021-04-21 20:59:52 -0400

    feat: add Group schema & groupId to User schema
    Also create a group method on the User schema so the group object is
    returned instead of the group ID.

--- models.js
+++ models.js
@@ -3,14 +3,30 @@ const mongoose = require('mongoose');
 // Connect to the local MongoDB named "testdb_graphql-guide".
 mongoose.connect('mongodb://localhost/testdb_graphql-guide');
 
+// Create a Group schema to be stored in the MongoDB database
+const GroupSchema = new mongoose.Schema({
+  _id: String,
+  name: String,
+});
+
+// Turn that schema into a model that we can query
+const Group = mongoose.model('Group', GroupSchema);
+
 // Create a User schema to be stored in the MongoDB database
 const UserSchema = new mongoose.Schema({
   _id: String,
+  groupId: String,
   name: String,
   username: String,
 });
 
+// Retrieve the group associated with the user
+UserSchema.methods.group = function () {
+  // Use .exec() to ensure a true Promise is returned
+  return Group.findById(this.groupId).exec();
+};
+
 // Turn that schema into a model that we can query
 const User = mongoose.model('User', UserSchema);
 
-module.exports = { User };
+module.exports = { Group, User };
